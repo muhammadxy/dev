0494b578db7c5607afef018c561a1e085e7d80fe
muhammadxy <muhammadayubaxy@gmail.com>
2022-05-17 10:15:48 +0100

    features: loggers

--- real/real/wsgi.py
+++ real/real/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real.settings.developments')
 
 application = get_wsgi_application()
